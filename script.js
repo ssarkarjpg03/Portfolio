9b08f448cb43298502bc681587ace4894530969b
ssarkarjpg03 <ssarkarjpg03@gmail.com>
2024-06-03 16:47:40 +0530

    education

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// $('.portfolio-button').mousedown(function(){
-//     timeout = setInterval(function(){
-//         window.scrollBy(0,-1); // May need to be -1 to go down
-//     }, 0); // Play around with this number. May go too fast
-
-//     return false;
-// });
 
 function showSidebar(){
     const sidebar = document.querySelector('.sidebar');
@@ -15,21 +8,6 @@ function disableSidebar(){
     sidebar = document.querySelector('.sidebar')
     sidebar.style.display = "none";
 }
-
-// function toggleTheme() {
-//     const body = document.body;
-
-//     if (body.classList.contains('light-theme')) {
-//         // Switch to dark theme
-//         body.classList.remove('light-theme');
-//         body.classList.add('dark-theme');
-//     } else {
-//         // Switch to light theme
-//         body.classList.remove('dark-theme');
-//         body.classList.add('light-theme');
-//     }
-// }
-
     document.getElementById('icon2').addEventListener('click',function(event) {
     event.preventDefault();
     const body = document.body;
